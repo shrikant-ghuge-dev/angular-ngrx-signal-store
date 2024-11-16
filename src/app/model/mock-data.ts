a16fe82a97a4d9799b5e5c23a425fd6d39dc3be1
Shrikant Ghuge <shreeghuge1@gmail.com>
2024-11-16 12:45:18 +0530

    Angular app initialization and created model and test data

---
+++ src/app/model/mock-data.ts
@@ -0,0 +1,14 @@
+import { Todo } from "./todo.model";
+
+export const TODOS: Todo[] = [
+    { id: "1", title: "Learn Angular", completed: false },
+    { id: "2", title: "Learn Angular", completed: false },
+    { id: "3", title: "Learn Angular", completed: false },
+    { id: "4", title: "Learn Angular", completed: false },
+    { id: "5", title: "Learn Angular", completed: false },
+    { id: "6", title: "Learn Angular", completed: false },
+    { id: "7", title: "Learn Angular", completed: false },
+    { id: "8", title: "Learn Angular", completed: false },
+    { id: "9", title: "Learn Angular", completed: false },
+    { id: "10", title: "Learn Angular", completed: false },
+];
